acf50524348e1718ad26c30f8453e319d3d35a83
NyrekWheeler <142206465+Nyrek2001@users.noreply.github.com>
2025-02-18 12:44:17 -0500

    Update UnitTest1.cs

--- supplementProj6.Tests/UnitTest1.cs
+++ supplementProj6.Tests/UnitTest1.cs
@@ -1,10 +1,11 @@
-﻿namespace supplementProj6.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
-    {
-
-    }
+  [Fact]
+public void Cube_GetVolume_CorrectResult()
+{
+    var cube = new Cube(3);
+    Assert.Equal(27, cube.GetVolume());
+}  
 }
+
